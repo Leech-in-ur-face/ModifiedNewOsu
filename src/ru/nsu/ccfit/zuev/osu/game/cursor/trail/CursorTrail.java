11737f62a1a9f4a4f26a05c81ff82e55eb859570
Leech-in-ur-face <74518523+Leech-in-ur-face@users.noreply.github.com>
2024-07-15 11:15:54 +0800

    Update CursorTrail.java

--- src/ru/nsu/ccfit/zuev/osu/game/cursor/trail/CursorTrail.java
+++ src/ru/nsu/ccfit/zuev/osu/game/cursor/trail/CursorTrail.java
@@ -5,10 +5,9 @@ import org.anddev.andengine.entity.particle.emitter.PointParticleEmitter;
 import org.anddev.andengine.entity.particle.initializer.ScaleInitializer;
 import org.anddev.andengine.entity.particle.modifier.AlphaModifier;
 import org.anddev.andengine.entity.particle.modifier.ExpireModifier;
-import org.anddev.andengine.entity.particle.modifier.ScaleModifier;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
 
-import android.opengl.GLES20;
+import javax.microedition.khronos.opengles.GL10;
 
 import ru.nsu.ccfit.zuev.osu.Config;
 import ru.nsu.ccfit.zuev.osu.game.cursor.main.CursorSprite;
@@ -23,14 +22,13 @@ public class CursorTrail extends ParticleSystem {
         super(emitter, spawnRate, spawnRate, spawnRate, pTextureRegion);
 
         fadeOut();
-        setBlendFunction(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
+        setBlendFunction(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
         addParticleInitializer(new ScaleInitializer(trailSize));
         setParticlesSpawnEnabled(false);
     }
 
     private void fadeOut() {
-        addParticleModifier(new ExpireModifier(0.40f));
-        addParticleModifier(new AlphaModifier(1.0f, 0.0f, 0.0f, 0.145f));
-        addParticleModifier(new ScaleModifier(1.0f, 0.75f, 0.0f, 1f));
+        addParticleModifier(new ExpireModifier(0.30f));
+        addParticleModifier(new AlphaModifier(1.0f, 0.0f, 0f, 0.120f));
     }
 }
